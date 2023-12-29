b4f9b96842f9ddc6da20633c34778b7ad517d761
José Braga <jose.braga@appliedblockchain.com>
2023-12-29 14:15:06 +0000

    Add C++ example

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# elliptic-hmac-cpp
-Elliptic HMAC implementation with C++
+# Elliptic HMAC implementation using C/C++ library
+
+The idea is to mimic the example in Golang provided in Elliptic
+[docs](https://developers.elliptic.co/docs/authentication) using C/C++ library.
+
+## Usage
+```bash
+    cmake -B build -S .
+    cmake --build build
+    ./build/elliptic
+```
---
+++ main.cpp
@@ -0,0 +1,110 @@
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+// libsecp256k1
+#include "src/hash_impl.h"
+#include "src/testrand_impl.h"
+
+static constexpr int TEST_SIZE = 7;
+
+// clang-format off
+// Copied from secp256k1/src/tests.c
+static void run_hmac_sha256_tests(void) {
+    static const char *keys[TEST_SIZE] = {
+        "\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b",
+        "\x4a\x65\x66\x65",
+        "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa",
+        "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19",
+        "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa",
+        "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa",
+        "this-is-my-key"
+    };
+    static const char *inputs[TEST_SIZE] = {
+        "\x48\x69\x20\x54\x68\x65\x72\x65",
+        "\x77\x68\x61\x74\x20\x64\x6f\x20\x79\x61\x20\x77\x61\x6e\x74\x20\x66\x6f\x72\x20\x6e\x6f\x74\x68\x69\x6e\x67\x3f",
+        "\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd\xdd",
+        "\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd",
+        "\x54\x65\x73\x74\x20\x55\x73\x69\x6e\x67\x20\x4c\x61\x72\x67\x65\x72\x20\x54\x68\x61\x6e\x20\x42\x6c\x6f\x63\x6b\x2d\x53\x69\x7a\x65\x20\x4b\x65\x79\x20\x2d\x20\x48\x61\x73\x68\x20\x4b\x65\x79\x20\x46\x69\x72\x73\x74",
+        "\x54\x68\x69\x73\x20\x69\x73\x20\x61\x20\x74\x65\x73\x74\x20\x75\x73\x69\x6e\x67\x20\x61\x20\x6c\x61\x72\x67\x65\x72\x20\x74\x68\x61\x6e\x20\x62\x6c\x6f\x63\x6b\x2d\x73\x69\x7a\x65\x20\x6b\x65\x79\x20\x61\x6e\x64\x20\x61\x20\x6c\x61\x72\x67\x65\x72\x20\x74\x68\x61\x6e\x20\x62\x6c\x6f\x63\x6b\x2d\x73\x69\x7a\x65\x20\x64\x61\x74\x61\x2e\x20\x54\x68\x65\x20\x6b\x65\x79\x20\x6e\x65\x65\x64\x73\x20\x74\x6f\x20\x62\x65\x20\x68\x61\x73\x68\x65\x64\x20\x62\x65\x66\x6f\x72\x65\x20\x62\x65\x69\x6e\x67\x20\x75\x73\x65\x64\x20\x62\x79\x20\x74\x68\x65\x20\x48\x4d\x41\x43\x20\x61\x6c\x67\x6f\x72\x69\x74\x68\x6d\x2e",
+        "this-is-my-text"
+    };
+    static const unsigned char outputs[TEST_SIZE][32] = {
+        {0xb0, 0x34, 0x4c, 0x61, 0xd8, 0xdb, 0x38, 0x53, 0x5c, 0xa8, 0xaf, 0xce, 0xaf, 0x0b, 0xf1, 0x2b, 0x88, 0x1d, 0xc2, 0x00, 0xc9, 0x83, 0x3d, 0xa7, 0x26, 0xe9, 0x37, 0x6c, 0x2e, 0x32, 0xcf, 0xf7},
+        {0x5b, 0xdc, 0xc1, 0x46, 0xbf, 0x60, 0x75, 0x4e, 0x6a, 0x04, 0x24, 0x26, 0x08, 0x95, 0x75, 0xc7, 0x5a, 0x00, 0x3f, 0x08, 0x9d, 0x27, 0x39, 0x83, 0x9d, 0xec, 0x58, 0xb9, 0x64, 0xec, 0x38, 0x43},
+        {0x77, 0x3e, 0xa9, 0x1e, 0x36, 0x80, 0x0e, 0x46, 0x85, 0x4d, 0xb8, 0xeb, 0xd0, 0x91, 0x81, 0xa7, 0x29, 0x59, 0x09, 0x8b, 0x3e, 0xf8, 0xc1, 0x22, 0xd9, 0x63, 0x55, 0x14, 0xce, 0xd5, 0x65, 0xfe},
+        {0x82, 0x55, 0x8a, 0x38, 0x9a, 0x44, 0x3c, 0x0e, 0xa4, 0xcc, 0x81, 0x98, 0x99, 0xf2, 0x08, 0x3a, 0x85, 0xf0, 0xfa, 0xa3, 0xe5, 0x78, 0xf8, 0x07, 0x7a, 0x2e, 0x3f, 0xf4, 0x67, 0x29, 0x66, 0x5b},
+        {0x60, 0xe4, 0x31, 0x59, 0x1e, 0xe0, 0xb6, 0x7f, 0x0d, 0x8a, 0x26, 0xaa, 0xcb, 0xf5, 0xb7, 0x7f, 0x8e, 0x0b, 0xc6, 0x21, 0x37, 0x28, 0xc5, 0x14, 0x05, 0x46, 0x04, 0x0f, 0x0e, 0xe3, 0x7f, 0x54},
+        {0x9b, 0x09, 0xff, 0xa7, 0x1b, 0x94, 0x2f, 0xcb, 0x27, 0x63, 0x5f, 0xbc, 0xd5, 0xb0, 0xe9, 0x44, 0xbf, 0xdc, 0x63, 0x64, 0x4f, 0x07, 0x13, 0x93, 0x8a, 0x7f, 0x51, 0x53, 0x5c, 0x3a, 0x35, 0xe2},
+        {0x4e, 0x92, 0x2b, 0x0d, 0xfe, 0x4f, 0x42, 0x3f, 0x05, 0xab, 0x35, 0x3e, 0xac, 0xdb, 0x17, 0x7b, 0x64, 0x5f, 0x0b, 0x36, 0x42, 0xb2, 0x06, 0xa2, 0xe7, 0xc4, 0x09, 0x1a, 0x36, 0xdb, 0xeb, 0x20}
+    };
+    int i;
+    for (i = 0; i < TEST_SIZE; i++) {
+        secp256k1_hmac_sha256 hasher;
+        unsigned char out[32];
+        secp256k1_hmac_sha256_initialize(&hasher, (const unsigned char*)(keys[i]), strlen(keys[i]));
+        secp256k1_hmac_sha256_write(&hasher, (const unsigned char*)(inputs[i]), strlen(inputs[i]));
+        secp256k1_hmac_sha256_finalize(&hasher, out);
+        CHECK(secp256k1_memcmp_var(out, outputs[i], 32) == 0);
+        if (strlen(inputs[i]) > 0) {
+            int split = secp256k1_testrand_int(strlen(inputs[i]));
+            secp256k1_hmac_sha256_initialize(&hasher, (const unsigned char*)(keys[i]), strlen(keys[i]));
+            secp256k1_hmac_sha256_write(&hasher, (const unsigned char*)(inputs[i]), split);
+            secp256k1_hmac_sha256_write(&hasher, (const unsigned char*)(inputs[i] + split), strlen(inputs[i]) - split);
+            secp256k1_hmac_sha256_finalize(&hasher, out);
+            CHECK(secp256k1_memcmp_var(out, outputs[i], 32) == 0);
+        }
+    }
+}
+// clang-format on
+
+void print_signature(const std::string &api_key,
+                     long unsigned time_of_request,
+                     const std::string &http_method,
+                     const std::string &http_path,
+                     const std::string &body)
+{
+    secp256k1_hmac_sha256 hasher;
+    unsigned char out[32];
+    secp256k1_hmac_sha256_initialize(
+        &hasher, (const unsigned char *)api_key.data(), api_key.size());
+
+    const std::string request = std::to_string(time_of_request) + http_method + http_path + body;
+    std::cout << "Request is:\n" << request << std::endl;
+    secp256k1_hmac_sha256_write(&hasher, (const unsigned char *)request.data(), request.size());
+    secp256k1_hmac_sha256_finalize(&hasher, out);
+
+    std::cout << "Signature is:\n";
+    for (const auto &c : out)
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<unsigned int>(c);
+    std::cout << "\n" << std::endl;
+}
+
+int main()
+{
+    std::cout << "running hmac_sha256 tests..." << std::endl;
+    run_hmac_sha256_tests();
+    std::cout << "...done!\n" << std::endl;
+
+    // Documentation: https://developers.elliptic.co/docs/authentication
+
+    // Elliptic API Key (hex format)
+    // base64: 894f142d667e8cdaca6822ac173937af
+    // hex: f3de1fd78d9debaedef1c75a71aebcdb669cd7bdfddfb69f
+    const std::string api_key =
+        "\xf3\xde\x1f\xd7\x8d\x9d\xeb\xae\xde\xf1\xc7\x5a\x71\xae\xbc\xdb\x66\x9c"
+        "\xd7\xbd\xfd\xdf\xb6\x9f";
+
+    long unsigned time_of_request = 1478692862000;
+    const std::string example_payload =
+        R"([{"customer_reference":"123456","subject":{"asset":"BTC","hash":"accf5c09cc027339a3beb2e28104ce9f406ecbbd29775b4a1a17ba213f1e035e","output_address":"15Hm2UEPaEuiAmgyNgd5mF3wugqLsYs3Wn","output_type":"address","type":"transaction"},"type":"source_of_funds"}])";
+
+    // base64: 65mQHB2o95lL3I+N/bZYwDC9p2YvNwsVDnXr8u72hUk=
+    // hex: eb99901c1da8f7994bdc8f8dfdb658c030bda7662f370b150e75ebf2eef68549
+    print_signature(api_key, time_of_request, "POST", "/v2/analyses", example_payload);
+
+    // base64: cN9fRUqeT7UnwwpkBZaNmnwxKAPHkhytdXelfUVvxMI=
+    // hex: 70df5f454a9e4fb527c30a6405968d9a7c312803c7921cad7577a57d456fc4c2
+    print_signature(api_key, time_of_request, "GET", "/v2/customers", "{}");
+}
